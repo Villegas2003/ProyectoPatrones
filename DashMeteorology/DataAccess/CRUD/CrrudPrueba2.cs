793670ba31a97a73d5d538894dbe0e3beb3bbce1
Jeremy Alejandro Villegas Rodriguez <jrodriguez118910511@gmail.com>
2023-11-05 18:26:21 -0600

    Commit 2 para ver cambios

--- DashMeteorology/DataAccess/CRUD/CrrudPrueba2.cs
+++ DashMeteorology/DataAccess/CRUD/CrrudPrueba2.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.CRUD
 {
-    internal class CrrudPrueba
+    internal class CrrudPrueba2
     {
     }
 }
